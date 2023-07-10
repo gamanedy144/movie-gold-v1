6fc18c5db7effcdca92039d4012597399c9210fa
gamanedy144 <gamanedy@yahoo.com>
2023-07-10 18:33:36 +0300

    fix header imports and add to main app component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,10 +2,9 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faVideoSlash } from '@fortawesome/free-solid-svg-icons';
 import { Button } from 'react-bootstrap';
-import Container from 'react-bootstrap';
-import Nav from 'react-bootstrap';
-import Navbar from 'react-bootstrap';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
+
 const Header = () => {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
